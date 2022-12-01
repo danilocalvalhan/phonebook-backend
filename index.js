bc9fd0d2ac5e1c580a637dd6e7b613b0138c8827
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-12-01 13:00:37 -0300

    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let persons = [
@@ -25,6 +26,7 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get("/info", (request, response) => {
 	const currentDate = new Date();
