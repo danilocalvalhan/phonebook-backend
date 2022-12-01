76b52ee114b1a6edcd324537a409cce6e3e1aa66
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-12-01 10:53:35 -0300

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ const persons = [
 	},
 ];
 
-app.get('/api/persons', (request, response) => {
-	response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+	response.json(persons);
+});
 
+app.get("/info", (request, response) => {
+	const currentDate = new Date();
+	response.send(`
+		<p>Phonebook has info for ${persons.length} people</p>
+		<p>${currentDate}</p>
+		`);
+});
 
 const PORT = 3001;
 app.listen(PORT);
