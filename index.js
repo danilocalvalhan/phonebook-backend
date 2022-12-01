e9b6695c8f5b9fad171f503d94422468da6131b1
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-12-01 11:20:05 -0300

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 
-const persons = [
+let persons = [
 	{
 		id: 1,
 		name: "Arto Hellas",
@@ -39,6 +39,13 @@ app.get("/api/persons/:id", (request, response) => {
 	const id = Number(request.params.id);
 	const person = persons.find((person) => person.id === id);
 	person ? response.json(person) : response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+
+	response.status(204).end();
 });
 
 const PORT = 3001;
