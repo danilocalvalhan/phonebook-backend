20697baa854a627a9350ce59faeff37f31df3fc4
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-12-19 16:50:23 -0300

    3.10/3.11 phonebook full stack

--- index.js
+++ index.js
@@ -97,6 +97,6 @@ app.post("/api/persons", (request, response) => {
 	}
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on PORT ${PORT}`);
