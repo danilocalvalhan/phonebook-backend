c4853c6632e8805a4036cb2af974ddbb371637e4
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-12-22 18:20:42 -0300

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -51,11 +51,15 @@ app.get("/api/persons/:id", (request, response) => {
 	.catch(err => response.status(404).end())
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((person) => person.id !== id);
-
-	response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		.then(result =>{
+			if (result) {
+				response.status(204).end();
+			} else {
+				response.status(404).end();
+			}
+		}).catch(error => next(error))
 });
 
 app.post("/api/persons", (request, response) => {
