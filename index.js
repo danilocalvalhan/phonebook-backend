2338d6c443dacb66e3222cf799cc5bb08a60918c
Danilo_Calvalhan <danilocalvalhan@hotmail.com>
2022-12-22 18:31:31 -0300

    3.16: Phonebook database, step4

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const Person = require("./models/person"); 
+const Person = require("./models/person");
 const app = express();
 
 morgan.token("post_data", (request, response) => {
@@ -29,7 +29,7 @@ app.use(
 
 app.get("/info", (request, response) => {
 	const currentDate = new Date();
-	Person.estimatedDocumentCount({}).then(count => {
+	Person.estimatedDocumentCount({}).then((count) => {
 		response.send(`
 			<p>Phonebook has info for ${count} people</p>
 			<p>${currentDate}</p>
@@ -38,45 +38,63 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-	Person.find({}).then(people => {
+	Person.find({}).then((people) => {
 		response.json(people);
-	})
+	});
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
 	const id = request.params.id;
-	Person.findById(id).then(person => {
-		response.json(person)
-	})
-	.catch(err => response.status(404).end())
+	Person.findById(id)
+		.then((person) => {
+			if (person) {
+				response.json(person);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
 	Person.findByIdAndRemove(request.params.id)
-		.then(result =>{
+		.then((result) => {
 			if (result) {
 				response.status(204).end();
 			} else {
 				response.status(404).end();
 			}
-		}).catch(error => next(error))
+		})
+		.catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
 	const newPerson = new Person({
 		name: request.body.name,
 		number: request.body.number,
-	})
+	});
 	if (newPerson.name == false || newPerson.number == false) {
 		return response.status(400).json({
 			error: "name or number is missing",
 		});
 	} else {
-		newPerson.save().then(result => {
+		newPerson.save().then((result) => {
 			return response.status(201).json(newPerson);
-		})
+		});
 	}
 });
+
+const errorHandler = (error, request, response, next) => {
+	console.log(error);
+
+	if (error.name === "CastError") {
+		response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
